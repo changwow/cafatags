e7877ff3fc42cf1e861bdbd7d001ab12c2c9d185
cc <changwow@126.com>
2015-11-14 12:14:49 +0800

    change ways to display

--- cafatags/views.py
+++ cafatags/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Txt
 
 def home(request):
-	tagsList = Txt.objects.all()
+	tagsList = Txt.objects.all()[:25:-1]
 	return render(request,'home.html', {'tagsList':tagsList})
 
 # Create your views here.
