9307b94b1956114cd7c4603dbb598e9a26cf513f
cc <changwow@126.com>
2015-11-14 11:25:29 +0800

    load data from db

--- cafatags/views.py
+++ cafatags/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Txt
 
 def home(request):
-	return render(request,'home.html')
+	tagsList = Txt.objects.all()
+	return render(request,'home.html', {'tagsList':tagsList})
 
 # Create your views here.
